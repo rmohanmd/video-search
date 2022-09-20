a64c96b61cce0dd91fc530b54e8b274c4eaa2ff1
rmohanmd <rmohan.md@gmail.com>
2022-09-20 17:49:49 -0400

    Setup youtube api file with axios

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyD5fAtNynpAXan0mO6V4Rfaw9uUuaIdRG8";
+
+export default axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
