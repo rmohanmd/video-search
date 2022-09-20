5fc49d868c20aca9bb197fa955f99748f0e1b6be
rmohanmd <rmohan.md@gmail.com>
2022-09-20 17:29:21 -0400

    Created a controlled form & added onSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,17 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onInputChange = () => {};
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
 
+  onFormSubmit = (event) => {
+    event.preventDeafault();
+  };
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input
