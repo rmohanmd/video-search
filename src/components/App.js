b23a5790b9c1b7e8c2919b6048a95321f30dae38
rmohanmd <rmohan.md@gmail.com>
2022-09-20 18:05:50 -0400

    Add method to obtain youtube api response

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    console.log(term);
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    console.log(response);
   };
 
   render() {
