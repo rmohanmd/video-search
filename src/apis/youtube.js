4964eb49ec28a92ce0e6a518c3e6c544ce88a2cf
rmohanmd <rmohan.md@gmail.com>
2022-09-20 17:52:28 -0400

    Added 'videos' only param to youtube api request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,6 +6,7 @@ export default axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
+    type: "videos",
     maxResults: 5,
     key: KEY,
   },
